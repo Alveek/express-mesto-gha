9a93fb39fa523494738f866c0ea76fa9ef2a7a48
alveek <alveek@gmail.com>
2023-04-22 23:07:28 +0300

    testing link regex

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,6 @@
-const urlRegEx =
-  /^((http|https):\/\/.)[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)$/;
+const urlRegEx = /^(ftp|http|https):\/\/[^ "]+$/;
+
+// const urlRegEx = /^((http|https)://)[-a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)$/;
 
 module.exports = {
   urlRegEx,
