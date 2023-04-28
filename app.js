92a13794e1d2ef75d5ca7a94825c994be293d5a8
Alexey Kudryavtsev <alveek@yandex.ru>
2023-04-29 00:17:51 +0300

    add log files

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { errors } = require('celebrate');
 const authRouter = require('./routes/auth');
 const router = require('./routes');
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -14,10 +15,14 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(requestLogger);
+
 app.use(authRouter);
 
 app.use(auth);
 app.use(router);
+
+app.use(errorLogger);
 
 app.use(errors());
 
