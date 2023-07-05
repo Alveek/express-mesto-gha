f1ba4ed160be2a480dc2ecbdde8c918352534e10
Alexey Kudryavtsev <alveek@yandex.ru>
2023-07-05 17:40:13 +0300

    fix typo

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const authRouter = require('express').Router();
 const { celebrate } = require('celebrate');
 const { login, createUser } = require('../controllers/users');
-const { signinValidation, signupValidation } = require('../validatioin');
+const { signinValidation, signupValidation } = require('../validation');
 
 authRouter.post('/signin', celebrate(signinValidation), login);
 authRouter.post('/signup', celebrate(signupValidation), createUser);
